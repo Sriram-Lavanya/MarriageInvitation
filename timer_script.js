480428a8905686bc0d303f2f2952d14a40e8b9c5
Kalaivani-Mohan <123948790+Kalaivani-Mohan@users.noreply.github.com>
2023-03-25 11:30:28 +0530

    Update timer_script.js

--- timer_script.js
+++ timer_script.js
@@ -3,7 +3,7 @@ const hourselement = document.getElementById("hours");
 const minuteselement = document.getElementById("minutes");
 const secondselement = document.getElementById("seconds");
 
-const newYears = "25 Apr 2023";
+const newYears = "26 Apr 2023 06:00:00";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
